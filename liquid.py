4b12e56b013fcfe60b9dfc48e58b319d2f87ac41
dubhunter <the@dubhunter.com>
2014-01-02 03:33:00 -0800

    try floats

--- liquid.py
+++ liquid.py
@@ -8,5 +8,5 @@ sensor_liquid = Sensor(spi, ADC_LIQUID)
 while 1:
     sensor_liquid.read()
     reading = sensor_liquid.value()
-    print (512 - reading) / (512 - 138) * 100
+    print (512.0 - reading) / (512 - 138) * 100.0
     time.sleep(0.5)
