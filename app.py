2886ba55c3580e8d32c1d70bc3aab0cf03d47a11
dubhunter <the@dubhunter.com>
2014-01-07 02:30:51 -0800

    try it from python

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import sys
 from lib.aquapi import AquaPi
+
+log = open('/var/log/aqua-pi.log', 'w')
+sys.stdout = log
+sys.stderr = log
 
 AquaPi.DEBUG = True
 aquapi = AquaPi()
