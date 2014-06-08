f881c58037d4d2ffbc26e8e78a8b646f62aa75d1
dubhunter <the@dubhunter.com>
2014-06-07 23:53:54 -0700

    print the actual reading

--- liquid.py
+++ liquid.py
@@ -8,5 +8,6 @@ sensor_liquid = Sensor(spi, ADC_LIQUID)
 while 1:
     sensor_liquid.read()
     reading = sensor_liquid.value()
+    print reading
     print (512.0 - reading) / (512 - 138) * 100.0
     time.sleep(0.5)
