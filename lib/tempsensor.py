5ab39c754c4c8992c8210a255a49cb94c49fe109
dubhunter <the@dubhunter.com>
2016-01-17 03:39:22 -0800

    fix sensor formula

--- lib/tempsensor.py
+++ lib/tempsensor.py
@@ -10,8 +10,8 @@ class TempSensor(AnalogSensor):
     """
 
     def centigrade(self):
-        voltage = self.value() / 1024.0
-        # Convert from 10 mv per degree wit 500 mV offset to degrees ((voltage - 500mV) times 100)
+        voltage = self.value() * 5.0 / 1024.0
+        # Convert from 10 mv per degree with 500 mV offset to degrees ((voltage - 500mV) times 100)
         return (voltage - 0.5) * 100
 
     def fahrenheit(self):
